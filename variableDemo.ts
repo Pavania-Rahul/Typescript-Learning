d2ecc10521a7f1d3d0b69d260724ab7be4cee839
Pavania-Rahul <rahulpavania@gmail.com>
2026-07-03 16:40:35 +0530

    add diffrent types of variable

---
+++ variableDemo.ts
@@ -0,0 +1,22 @@
+// Defined Diffrent Types of Varibale 
+ 
+//string type--->>>
+
+var userName:string = "Test";
+console.log(userName);
+
+//number type it is accepted decimal and int number value-->>
+
+var numberValue:number= 12;
+console.log(numberValue);
+
+
+//booleanType--->>
+var isTrue:boolean =true;
+console.log(isTrue);
+
+
+//undefined type--->>
+
+var userData:undefined;
+console.log(userData);
