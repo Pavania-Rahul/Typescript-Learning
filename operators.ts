67141f2f1b47eaf81937ebf8dfc5d73bb545f45e
Pavania-Rahul <rahulpavania@gmail.com>
2026-07-03 18:35:29 +0530

    Add operator file

---
+++ operators.ts
@@ -0,0 +1,47 @@
+
+//Arithmatic operator
+var x:number = 20;
+var y:number = 10;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(5**2);
+
+//Assignment operator 
+console.log(x+=1);
+console.log(x-=1);
+console.log(x*=2);
+
+//Comparison operator
+
+var x:number = 20;
+var y:number =10;
+
+console.log(x>y);
+console.log(x<y);
+console.log(x>=y);
+console.log(x<=y);
+console.log(x!=y);
+
+if(x>y){
+    console.log("X is big number "+x);
+}
+
+function age(value:number){
+    if(value>=18){
+        console.log("You are eligible for vote your age is::"+value);
+    }else{
+        console.log("you are not eligible for vote please wait until you have 18 years old::"+value);
+    }
+}
+
+age(2);
+
+
+//turnary operator
+var firstDeclare:number = 10;
+var secondDeclare:number = 20;
+console.log(firstDeclare>secondDeclare?"first number is big":"second number is big");
