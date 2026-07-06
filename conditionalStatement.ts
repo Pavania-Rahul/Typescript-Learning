3f499998dbcb77a802aa357600c757ff04dbdb0f
Pavania-Rahul <rahulpavania@gmail.com>
2026-07-06 11:51:33 +0530

    Added Loop

--- conditionalStatement.ts
+++ conditionalStatement.ts
@@ -13,7 +13,7 @@ checkEligiblity(18); // --->> Eligible for vote
 
 
 
-//muliple condition:
+//Multiple condition:
 var valueA:number = 10;
 var valueB:number =20;
 var valueC:number =30;
@@ -24,4 +24,48 @@ if(valueA>valueB && valueA>valueC){
     console.log("Value B is bigger");
 }else{
     console.log("Value C is bigger");   //-->> Value C is bigger
+}
+
+
+
+//Ternary Operator
+var x:number = 100;
+var y:number = 200;
+x>y? console.log("x is largest number"): console.log("y is largest number"); //-->> y is largest number
+
+
+
+//Nested If else condition
+var weekNo:number =1;
+
+  if(weekNo==1){
+    console.log("Sunday");  //-->> Sunday
+  }else if(weekNo==2){
+    console.log("Monday");
+  }else if(weekNo==3){
+    console.log("Tuesday");
+  }else if(weekNo==4){
+    console.log("Wednesday");
+  }else if(weekNo==5){
+    console.log("Thursday");
+  }else if(weekNo==6){
+    console.log("Friday");
+  }else if(weekNo==7){
+    console.log("Saturday");
+  }else{
+    console.log("Invalid input");
+  }
+
+
+//Switch case
+var weekNo:number = 3;
+switch(weekNo){
+ case 1: console.log("Sunday"); break;
+ case 2: console.log("Monday"); break;
+ case 3: console.log("Tuesday"); break; //---> Tuesday
+ case 4: console.log("Wednesday"); break;
+ case 5: console.log("Thursday"); break;
+ case 6: console.log("Friday"); break;
+ case 7: console.log("Saturdat"); break;
+ default:console.log("Invalid output"); break;
 }
---
+++ loops.ts
@@ -0,0 +1,39 @@
+//While loop
+var i:number =1;
+while(i<=10){
+     console.log(i); //12345678910
+     i++;
+}
+
+
+var i:number =2;
+while(i<=10){
+    console.log(i); //increment with 2  so output will be -->> 246810
+    i+=2;
+}
+
+var i:number =10;
+
+while(i>0){
+  console.log(i); // ---->>10987654321
+  i--;
+}
+
+
+
+//do while loop
+var i:number =1;
+do{
+    console.log(i); //12345678910
+    i++;
+
+}while(i<=10);
+
+
+
+//For Loop
+
+for(var i:number=11;i<=20;i++){
+    console.log(i); //--->> 11121314151617181920
+}
+
